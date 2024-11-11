e4e9163a1e7f055e160609e7ef58f9792dc2ccd0
levismim <86032562+levismim@users.noreply.github.com>
2024-11-11 14:19:00 -0600

    simplify operator functions

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-function add(a, b) {
-    const result = a + b;
-    return result;
-}
+add = (a, b) => a + b;
+subtract = (a, b) => a - b;
+multiply = (a, b) => a * b;
+divide = (a, b) => a / b;
 
-function subtract(a, b) {
-    const result = a - b;
-    return result;
-}
-
-function multiply(a, b) {
-    const result = a * b;
-    return result;
-}
-
-function divide(a, b) {
-    const result = a / b;
-    return result;
-}
 
 let num1;
 let op;
 let num2;
-
+const plus = document.getElementById("add");
+const minus = document.getElementById("subtract");
+const times = document.getElementById("multiply");
+const division = document.getElementById("divide");
+const display = document.getElementsByClassName("display")
+const clear = document.getElementById("clear")
 function operate(op, a, b) {
     if(op = "+") {
         add(a, b)
@@ -34,18 +25,19 @@ function operate(op, a, b) {
     }
 };
 
-const plus = document.getElementById("add");
-const minus = document.getElementById("subtract");
-const times = document.getElementById("multiply");
-const division = document.getElementById("divide");
-const display = document.getElementsByClassName("display")
+clear.addEventListener("click", function() {
+    display[0].textContent = " "
+})
 //when you click a number
 //empty display
 //populate display with number
+//store number in a variable
 const numbtns = document.querySelectorAll('.num')
 
 numbtns.forEach(btn => {
     btn.addEventListener('click', function(){
-        display[0].textContent = btn.id;
+    display[0].textContent = btn.id;
+    let num = btn.id;
+    return num;
     })
 })

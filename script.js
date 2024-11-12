b87d2f8f3c5d62ad79626e5f8ab7333c9dbd8534
levismim <86032562+levismim@users.noreply.github.com>
2024-11-12 10:42:40 -0600

    removed button functions to start over

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ multiply = (a, b) => a * b;
 divide = (a, b) => a / b;
 
 
-let num1;
+let num1 = 0;
 let op;
-let num2;
+let num2 = 0;
 const plus = document.getElementById("add");
 const minus = document.getElementById("subtract");
 const times = document.getElementById("multiply");
 const division = document.getElementById("divide");
 const display = document.getElementsByClassName("display")
 const clear = document.getElementById("clear")
-function operate(op, a, b) {
+
+operate = (op, a, b) => {
     if(op = "+") {
         add(a, b)
     } else if(op = "-") {
@@ -24,20 +25,3 @@ function operate(op, a, b) {
         divide(a, b)
     }
 };
-
-clear.addEventListener("click", function() {
-    display[0].textContent = " "
-})
-//when you click a number
-//empty display
-//populate display with number
-//store number in a variable
-const numbtns = document.querySelectorAll('.num')
-
-numbtns.forEach(btn => {
-    btn.addEventListener('click', function(){
-    display[0].textContent = btn.id;
-    let num = btn.id;
-    return num;
-    })
-})

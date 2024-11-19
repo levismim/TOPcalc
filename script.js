d719e5976596b5c4f72f1a90b971f31f186276c3
levismim <86032562+levismim@users.noreply.github.com>
2024-11-18 18:15:00 -0600

    calculator mostly functional cannot use more than one operator yet will work on next commit

--- script.js
+++ script.js
@@ -1,45 +1,84 @@
 add = (a, b) => a + b;
 subtract = (a, b) => a - b;
 multiply = (a, b) => a * b;
-divide = (a, b) => a / b;
+function divide(a, b) { 
 
+    if (b == 0) {
+        result = "nope";
+    } else {
+        result = a / b;
+    } return result;
+    };
 
+let result = '';
 let num1 = '';
-let op;
+let operator = undefined;
 let num2 = '';
-const plus = document.getElementById("add");
-const minus = document.getElementById("subtract");
-const times = document.getElementById("multiply");
-const division = document.getElementById("divide");
-const display = document.getElementsByClassName("display")
-const clear = document.getElementById("clear")
+const display = document.getElementsByClassName("display");
+const clear = document.getElementById("clear");
 
 operate = (op, a, b) => {
-    if(op = "+") {
-        add(a, b)
-    } else if(op = "-") {
-        subtract(a, b)
-    } else if(op = "*") {
-        multiply(a, b)
-    } else if(op = "/") {
-        divide(a, b)
+    if(op == "+") {
+        result = add(a, b);
+        return result;
+    } else if(op == "-") {
+        result = subtract(a, b)
+        return result;
+    } else if(op == "*") {
+        result = multiply(a, b)
+        return result;
+    } else if(op == "/") {
+        result = divide(a, b)
+        return result;
     }
 };
 
 clear.addEventListener("click", function() {
-    display[0].textContent = "0"
-})
+    display[0].textContent = ''
+    num1 = '';
+    num2 = '';
+    operator = undefined;
+});
 //when you click a number
 //empty display
     //populate display with input
 //store number in a variable
-const numbtns = document.querySelectorAll('.num')
-
+const numbtns = document.querySelectorAll('.num');
+//if operator was clicked, store numbers in a new variable.
 numbtns.forEach(btn => {
     btn.addEventListener('click', function input(){
-        let finNum = num1 += btn.id;
-        display[0].textContent = finNum;
-        num1 = finNum;
-        return num1;    
+        if(operator === undefined) {
+            let firstNum = parseInt(num1 += btn.id);
+            display[0].textContent = firstNum;
+            num1 = firstNum;
+            console.log(num1)
+            return num1;    
+        } else {
+            let nextNum = parseInt(num2 += btn.id);
+            display[0].textContent = nextNum;
+            num2 = nextNum;
+            console.log(num2)
+            return num2;    
+    }
+})
+});
+
+const ops = document.querySelectorAll('.op')
+
+ops.forEach(op => {
+    op.addEventListener('click', function input(){
+        operator = op.id;
+        display[0].textContent = operator;
+        console.log(operator);
+        return operator;
     })
 })
+
+const equals = document.getElementById("equal");
+
+equals.addEventListener('click', function finalResult() {
+    display[0].textContent = '';
+operate(operator, num1, num2);
+display[0].textContent = result;
+num1 = result;
+});
